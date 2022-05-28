183b41ca9942c2831c24d7872a8bce76073c75c9
Marko Scekic <81862323+sceka@users.noreply.github.com>
2022-05-29 01:53:34 +0200

    Update Statistics Page (#8)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
-import LandingPage from './landing-page/LandingPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Metamask } from './Metamask/Metamask';
-import { ExplorePage } from './explore-page/ExplorePage';
+import LandingPage from "./landing-page/LandingPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Metamask } from "./Metamask/Metamask";
+import { ExplorePage } from "./explore-page/ExplorePage";
 
-import React from 'react';
-import { UserProvider } from '../provider/UserContext';
-import { NewPost } from './new-post-page/NewPost';
+import React from "react";
+import { UserProvider } from "../provider/UserContext";
+import { NewPost } from "./new-post-page/NewPost";
+import { Chart } from "./Statistics/Statistics";
 
 function App() {
-	return (
-		<UserProvider>
-			<Router>
-				<Routes>
-					<Route path='/' element={<LandingPage />} />
-					<Route path='/explore' element={<ExplorePage />} />
-					<Route path='/metamask' element={<Metamask />} />
-          <Route path='/new-post' element={<NewPost />} />
-				</Routes>
-			</Router>
-		</UserProvider>
-	);
+    return (
+        <UserProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/explore" element={<ExplorePage />} />
+                    <Route path="/metamask" element={<Metamask />} />
+                    <Route path="/new-post" element={<NewPost />} />
+                    <Route path="/statistics" element={<Chart />} />
+                </Routes>
+            </Router>
+        </UserProvider>
+    );
 }
 
 export default App;
