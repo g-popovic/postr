3a8f25c43fa7f02e8ab18812b4bcf356ad9b440b
George Popovic <georgepopovich14@gmail.com>
2022-05-28 17:48:13 +0200

    Finish fade-out animation

--- client/src/components/landing-page/LandingPage.jsx
+++ client/src/components/landing-page/LandingPage.jsx
@@ -1,19 +1,28 @@
 import './styles.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BackgroundImage from '../../assets/background.jpg';
 
 export default function LandingPage() {
 	const [showFirstSlide, setShowFirstSlide] = useState(true);
 	const [showSecondSlide, setShowSecondSlide] = useState(false);
+	const [showFadeOutAnimation, setShowFadeOutAnimation] = useState(false);
 
 	function transitionToSecondSlide() {
 		setShowFirstSlide(false);
 		setTimeout(() => setShowSecondSlide(true), 400);
 	}
 
+	function onPressExplore() {
+		setShowFadeOutAnimation(true);
+	}
+
 	return (
 		<div className='outer-container' onClick={transitionToSecondSlide}>
-			<img className='bg-image' src={BackgroundImage} alt='background' />
+			<img
+				className={'bg-image' + (showFadeOutAnimation ? ' fade-out' : '')}
+				src={BackgroundImage}
+				alt='background'
+			/>
 
 			<div className='title-container'>
 				<div className={!showFirstSlide && 'fade-out'}>
@@ -23,14 +32,23 @@ export default function LandingPage() {
 			</div>
 
 			<div className='info-container'>
-				<div className={!showSecondSlide && 'fade-out'}>
+				<div
+					className={
+						!showSecondSlide
+							? 'fade-out-pre-intro'
+							: showFadeOutAnimation
+							? 'fade-out-outro'
+							: ''
+					}>
 					<h5>POSTR</h5>
 					<p>
 						Hello world this is a text that is supposed to explain more about the
 						product but do not worry about it too much for now
 					</p>
 
-					<button className='btn btn-outline-light'>EXPLORE</button>
+					<button className='btn btn-outline-light' onClick={onPressExplore}>
+						EXPLORE
+					</button>
 				</div>
 			</div>
 		</div>
