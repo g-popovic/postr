276e1e264c0dcce2160f3aee3906ec89202738a8
Marko Scekic <81862323+sceka@users.noreply.github.com>
2022-05-28 18:30:26 +0200

    Implement Metamask (#2)
    * Implement Metamask
    
    * Change the spacing options
    
    * Fix merge errors
    
    Co-authored-by: George Popovic <georgepopovich14@gmail.com>

---
+++ client/src/components/Metamask/button.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import './Metamask.scss';
+import { injected } from './connectors';
+import icon from '../../statics/metamask-icon.png';
+import { useWeb3React } from '@web3-react/core';
+// import Web3 from "web3";
+export const MetamaskButton = () => {
+	//   const web3 = await new Web3().currentProvider;
+	const { active, account, library, connector, activate, deactivate } = useWeb3React();
+	async function connect() {
+		if (!window.ethereum) {
+			alert('Install Metamask');
+		} else {
+			try {
+				await activate(injected);
+				localStorage.setItem('isWalletConnected', true);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	//   async function disconnect() {
+	//     try {
+	//       await deactivate(injected);
+	//       localStorage.getItem("isWalletConnected", false);
+
+	//       //   await web3.eth.currentProvider.disconnect();
+	//     } catch (err) {
+	//       console.log(err);
+	//     }
+	//   }
+	useEffect(() => {
+		const connectWalletOnPageLoad = async () => {
+			if (localStorage?.getItem('isWalletConnected') === 'true') {
+				try {
+					await activate(injected);
+					localStorage.setItem('isWalletConnected', true);
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		};
+		connectWalletOnPageLoad();
+	}, []);
+
+	return (
+		<div>
+			<button className='container' onClick={connect}>
+				<div className='metamask-image-container'>
+					<img src={icon} alt='metamask' className='metamask-image' />
+				</div>
+				{active === false ? (
+					<div className='wallet-text'>Connect wallet</div>
+				) : (
+					<div className='wallet-connected'>{account.slice(0, 13) + '...'}</div>
+				)}
+			</button>
+		</div>
+	);
+};
