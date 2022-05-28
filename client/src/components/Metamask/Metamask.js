158bfc0d37f102c10a061436b89114568eec9390
sceka <scekicm02@gmail.com>
2022-05-28 18:21:59 +0200

    Change the spacing options

--- client/src/components/Metamask/Metamask.js
+++ client/src/components/Metamask/Metamask.js
@@ -4,16 +4,16 @@ import Web3 from "web3";
 import Button from "./button";
 
 const Metamask = () => {
-  function getLibrary(provider) {
-    return new Web3(provider);
-  }
-  return (
-    <div>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Button />
-      </Web3ReactProvider>
-    </div>
-  );
+    function getLibrary(provider) {
+        return new Web3(provider);
+    }
+    return (
+        <div>
+            <Web3ReactProvider getLibrary={getLibrary}>
+                <Button />
+            </Web3ReactProvider>
+        </div>
+    );
 };
 
 export default Metamask;
