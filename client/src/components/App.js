b3fa87d37c3badd564f9bab42a1f209f7c861258
Uros Stesevic <101807204+stesevicuros@users.noreply.github.com>
2022-05-28 20:58:23 +0200

    Create NewPost Page (#5)
    * Create NewPost Page
    
    * Minor Fixes
    
    Co-authored-by: George Popovic <georgepopovich14@gmail.com>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,10 @@ import LandingPage from './landing-page/LandingPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Metamask } from './Metamask/Metamask';
 import { ExplorePage } from './explore-page/ExplorePage';
+
 import React from 'react';
 import { UserProvider } from '../provider/UserContext';
+import { NewPost } from './new-post-page/NewPost';
 
 function App() {
 	return (
@@ -13,6 +15,7 @@ function App() {
 					<Route path='/' element={<LandingPage />} />
 					<Route path='/explore' element={<ExplorePage />} />
 					<Route path='/metamask' element={<Metamask />} />
+          <Route path='/new-post' element={<NewPost />} />
 				</Routes>
 			</Router>
 		</UserProvider>
