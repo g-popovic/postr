0ef8e6b250b1702fb26f6c77cd3108ce6c620ba2
George Popovic <georgepopovich14@gmail.com>
2022-05-28 18:15:51 +0200

    Implement browser router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,14 @@
 import LandingPage from './landing-page/LandingPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
-		<div>
-			<LandingPage />
-		</div>
+		<Router>
+			<Routes>
+				<Route path='/' element={<LandingPage />} />
+				<Route path='/home' element={<h1>hello</h1>} />
+			</Routes>
+		</Router>
 	);
 }
 
