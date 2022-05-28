b777098262c0daa6ede4ff08f1956f484952ef7b
George Popovic <georgepopovich14@gmail.com>
2022-05-28 21:28:06 +0200

    Create sidebar and fix up Post styles (#6)
    * Fix posts styling
    
    * create-sidebar

--- client/src/components/Reusable/Navbar.jsx
+++ client/src/components/Reusable/Navbar.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function Navbar() {
-    return (
-        <div className='navbar-wrapper'>
-            <h1 className='logo'>LOGO</h1>
-            <button className='menu-button'>BUTTON</button>
-        </div>
-    );
+	const [showSidebar, setShowSidebar] = useState(false);
+	const [sidebarIsClosing, setSidebarIsClosing] = useState(false);
+
+	function onOpenSidebar() {
+		setShowSidebar(true);
+		setSidebarIsClosing(false);
+		// document.querySelector("body").className += 'no-scroll'
+	}
+
+	function onCloseSidebar() {
+		setSidebarIsClosing(true);
+		// After closing animation plays:
+		setTimeout(() => setShowSidebar(false), 300);
+	}
+
+	return (
+		<>
+			<div className='navbar-wrapper'>
+				<h3 className='logo'>LOGO</h3>
+				<button className='menu-button' onClick={onOpenSidebar}>
+					MENU
+				</button>
+			</div>
+
+			{showSidebar && (
+				<div>
+					<div
+						onClick={onCloseSidebar}
+						className={
+							'sidebar-background-blur' + (sidebarIsClosing ? ' closing' : '')
+						}></div>
+					<div className={'sidebar-container' + (sidebarIsClosing ? ' closing' : '')}>
+						<h5 className='text-white'>LOGO</h5>
+
+						<div className='d-flex flex-column sidebar-btn-containers'>
+							<button className='btn btn-outline-light'>BUTTON 1</button>
+							<button className='btn btn-outline-light'>BUTTON 2</button>
+							<button className='btn btn-outline-light'>BUTTON 3</button>
+							<button className='btn btn-outline-light'>BUTTON 4</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
--- client/src/components/Reusable/Post.jsx
+++ client/src/components/Reusable/Post.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 export function Post() {
-    return (
-        <div className='wrapper'>
-            <div className='title-container'>
-                <img className='profile-img' />
-                <h1 className='username'>Uros Stesevic</h1>
-            </div>
-            <span className='post-content'>
-                Lorem ipsum is placeholder text commonly used in the graphic,
-                print, and publishing industries for previewing layouts and
-                visual mockups.
-            </span>
-            <div className='likes'>
-                <i class='fa-regular fa-heart'></i> <span>234 Likes</span>
-            </div>
-        </div>
-    );
+	return (
+		<div className='wrapper'>
+			<div className='username-container'>
+				<img
+					className='profile-img'
+					src='https://i1.sndcdn.com/avatars-000508491087-32hktm-t500x500.jpg'
+					alt='avatar'
+				/>
+				<p className='username'>Uros Stesevic</p>
+			</div>
+			<p className='post-content font-weight-bold'>
+				Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing
+				industries for previewing layouts and visual mockups.
+			</p>
+			<div className='likes'>
+				<i class='fa-regular fa-heart'></i> <span>234 Likes</span>
+			</div>
+		</div>
+	);
 }
