1bcf346849bf62904d277ca518e2067bb5d7b126
George Popovic <georgepopovich14@gmail.com>
2022-05-28 18:29:30 +0200

    Fix merge errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import LandingPage from './landing-page/LandingPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Metamask } from './Metamask/Metamask';
 
 function App() {
 	return (
@@ -7,6 +8,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
 				<Route path='/home' element={<h1>hello</h1>} />
+				<Route path='/metamask' element={<Metamask />} />
 			</Routes>
 		</Router>
 	);
--- client/src/components/Metamask/Metamask.js
+++ client/src/components/Metamask/Metamask.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { Web3ReactProvider } from "@web3-react/core";
-import Web3 from "web3";
-import Button from "./button";
+import React from 'react';
+import { Web3ReactProvider } from '@web3-react/core';
+import Web3 from 'web3';
+import { MetamaskButton } from './button';
 
-const Metamask = () => {
-    function getLibrary(provider) {
-        return new Web3(provider);
-    }
-    return (
-        <div>
-            <Web3ReactProvider getLibrary={getLibrary}>
-                <Button />
-            </Web3ReactProvider>
-        </div>
-    );
+export const Metamask = () => {
+	function getLibrary(provider) {
+		return new Web3(provider);
+	}
+	return (
+		<div>
+			<Web3ReactProvider getLibrary={getLibrary}>
+				<MetamaskButton />
+			</Web3ReactProvider>
+		</div>
+	);
 };
-
-export default Metamask;
