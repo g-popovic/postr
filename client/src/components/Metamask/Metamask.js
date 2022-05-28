1c96747f667c591025d6f46f40789db3356697bd
sceka <scekicm02@gmail.com>
2022-05-28 18:13:57 +0200

    Implement Metamask

---
+++ client/src/components/Metamask/Metamask.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+import Button from "./button";
+
+const Metamask = () => {
+  function getLibrary(provider) {
+    return new Web3(provider);
+  }
+  return (
+    <div>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Button />
+      </Web3ReactProvider>
+    </div>
+  );
+};
+
+export default Metamask;
