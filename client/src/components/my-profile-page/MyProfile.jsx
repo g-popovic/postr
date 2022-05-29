aa1ccdd3bb526cd0b621f9c0071bf20a282c2dd7
stesevicuros <stesevicu@gmail.com>
2022-05-29 02:02:59 +0200

    Fix Bugs

--- client/src/components/my-profile-page/MyProfile.jsx
+++ client/src/components/my-profile-page/MyProfile.jsx
@@ -22,58 +22,60 @@ export function MyProfile() {
 	}
 
 	return (
-		<div className='my-page-container'>
-			<Navbar />
-			<div className='bio'>
-				<img
-					className='bio-img'
-					src='https://i1.sndcdn.com/avatars-000508491087-32hktm-t500x500.jpg'
-					alt='avatar'
-				/>
-				<h1 className='bio-username'>Uros Stesevic</h1>
-				{!edit && (
-					<div>
-						<button
-							class='fa-solid fa-pen-to-square'
-							onClick={handleClick}
-						></button>
-					</div>
-				)}
-				{!edit && (
-					<div>
-						<p>
-							Lorem ipsum is placeholder text commonly used in the
-							graphic, print, and publishing industries for
-							previewing layouts and visual mockups.
-						</p>
-					</div>
-				)}
-				{edit && (
-					<div>
-						{/* <input
+		<>
+			<div className='my-page-container'>
+				<Navbar />
+				<div className='bio'>
+					<img
+						className='bio-img'
+						src='https://i1.sndcdn.com/avatars-000508491087-32hktm-t500x500.jpg'
+						alt='avatar'
+					/>
+					<h1 className='bio-username'>Uros Stesevic</h1>
+					{!edit && (
+						<div>
+							<button
+								class='fa-solid fa-pen-to-square'
+								onClick={handleClick}
+							></button>
+						</div>
+					)}
+					{!edit && (
+						<div>
+							<p>
+								Lorem ipsum is placeholder text commonly used in
+								the graphic, print, and publishing industries
+								for previewing layouts and visual mockups.
+							</p>
+						</div>
+					)}
+					{edit && (
+						<div>
+							{/* <input
 							type='text'
 							onChange={handleUserNameChange}
 							value={userNameValue}
 							autoComplete='off'
 						/> */}
-						<textarea
-							type='text'
-							onChange={handleTextChange}
-							value={textValue}
-							autoComplete='off'
-						/>
-						<div className='bio-buttons'>
-							<button>SAVE</button>
-							<button onClick={handleClick}>DISCARD</button>
+							<textarea
+								type='text'
+								onChange={handleTextChange}
+								value={textValue}
+								autoComplete='off'
+							/>
+							<div className='bio-buttons'>
+								<button>SAVE</button>
+								<button onClick={handleClick}>DISCARD</button>
+							</div>
 						</div>
-					</div>
-				)}
+					)}
+				</div>
+				<div className='posts-container'>
+					<Post />
+					<Post />
+					<Post />
+				</div>
 			</div>
-			<div className='posts-container'>
-				<Post />
-				<Post />
-				<Post />
-			</div>
-		</div>
+		</>
 	);
 }
