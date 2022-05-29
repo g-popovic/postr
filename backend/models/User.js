3cfeb2c1683bd251c3fe889d6b64b15e57834f89
George Popovic <georgepopovich14@gmail.com>
2022-05-29 08:01:27 +0200

    Refactor code

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const postSchema = require("./Post");
-const userSchema = new mongoose.Schema({
-    wallet: String,
-    name: String,
-    bio: String,
-});
-
-module.exports = mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
-const dotenv = require("dotenv").config();
-const User = require("./models/User");
-const Post = require("./models/Post");
+const dotenv = require('dotenv').config();
+const User = require('./models/user.model');
+const Post = require('./models/post.model');
 
 app.use(
-    cors({
-        origin: "*",
-    })
+	cors({
+		origin: '*',
+	}),
 );
 mongoose.connect(process.env.DATABASE_LINK, {
-    useNewUrlParser: true,
+	useNewUrlParser: true,
 });
 const db = mongoose.connection;
-db.on("err", (err) => console.error(err));
-db.once("open", () => console.log("Connected to MongoDB"));
+db.on('err', err => console.error(err));
+db.once('open', () => console.log('Connected to MongoDB'));
 // const user = new User({
 //     wallet: "0x1111111111",
 //     name: "Marko",
@@ -26,19 +26,19 @@ db.once("open", () => console.log("Connected to MongoDB"));
 // });
 // user.save().then(() => console.log("User is saved"));
 const post = new Post({
-    authorWallet: "0x1111111111",
-    likes: 1,
-    name: "Marko",
-    content: "Hello World I am Marko",
+	authorWallet: '0x1111111111',
+	likes: 1,
+	name: 'Marko',
+	content: 'Hello World I am Marko',
 });
 // post.save().then(() => console.log("Post is saved"));
-const indexRoute = require("./routes/index");
+const indexRoute = require('./routes/index');
 
-app.use(express.static("public"));
-console.log("GetPosts, CreatePosts, EditProfile, Like, Statisctis, login");
-app.use("/", indexRoute);
+app.use(express.static('public'));
+console.log('GetPosts, CreatePosts, EditProfile, Like, Statisctis, login');
+app.use('/', indexRoute);
 const port = 3001;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
