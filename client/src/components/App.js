dfb34f25a4fba95af6818ea3d167cdd43d74b44d
Uros Stesevic <101807204+stesevicuros@users.noreply.github.com>
2022-05-29 02:11:09 +0200

    Profile page (#9)
    * Inplement part of funcionality
    
    * Create MyProfile Page
    
    * Fix Bugs
    
    Co-authored-by: George Popovic <georgepopovich14@gmail.com>

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,9 @@
-import LandingPage from "./landing-page/LandingPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Metamask } from "./Metamask/Metamask";
-import { ExplorePage } from "./explore-page/ExplorePage";
 
 import React from "react";
 import { UserProvider } from "../provider/UserContext";
 import { NewPost } from "./new-post-page/NewPost";
 import { Chart } from "./Statistics/Statistics";
+import { MyProfile } from './my-profile-page/MyProfile';
 
 function App() {
     return (
@@ -18,6 +15,7 @@ function App() {
                     <Route path="/metamask" element={<Metamask />} />
                     <Route path="/new-post" element={<NewPost />} />
                     <Route path="/statistics" element={<Chart />} />
+					          <Route path='/my-profile' element={<MyProfile />} />
                 </Routes>
             </Router>
         </UserProvider>
---
+++ client/src/components/my-profile-page/MyProfile.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useState } from 'react';
+import { Navbar } from '../Reusable/Navbar';
+import { Post } from '../Reusable/Post';
+import './my-profile.scss';
+
+export function MyProfile() {
+	const [edit, setEdit] = useState(false);
+	const [textValue, setTextValue] = useState('');
+	const [userNameValue, setUserNameValue] = useState('');
+
+	function handleUserNameChange(e) {
+		setUserNameValue(e.target.value);
+	}
+
+	function handleTextChange(e) {
+		setTextValue(e.target.value);
+	}
+
+	function handleClick() {
+		setEdit((prevValue) => !prevValue);
+	}
+
+	return (
+		<>
+			<div className='my-page-container'>
+				<Navbar />
+				<div className='bio'>
+					<img
+						className='bio-img'
+						src='https://i1.sndcdn.com/avatars-000508491087-32hktm-t500x500.jpg'
+						alt='avatar'
+					/>
+					<h1 className='bio-username'>Uros Stesevic</h1>
+					{!edit && (
+						<div>
+							<button
+								class='fa-solid fa-pen-to-square'
+								onClick={handleClick}
+							></button>
+						</div>
+					)}
+					{!edit && (
+						<div>
+							<p>
+								Lorem ipsum is placeholder text commonly used in
+								the graphic, print, and publishing industries
+								for previewing layouts and visual mockups.
+							</p>
+						</div>
+					)}
+					{edit && (
+						<div>
+							{/* <input
+							type='text'
+							onChange={handleUserNameChange}
+							value={userNameValue}
+							autoComplete='off'
+						/> */}
+							<textarea
+								type='text'
+								onChange={handleTextChange}
+								value={textValue}
+								autoComplete='off'
+							/>
+							<div className='bio-buttons'>
+								<button>SAVE</button>
+								<button onClick={handleClick}>DISCARD</button>
+							</div>
+						</div>
+					)}
+				</div>
+				<div className='posts-container'>
+					<Post />
+					<Post />
+					<Post />
+				</div>
+			</div>
+		</>
+	);
+}
